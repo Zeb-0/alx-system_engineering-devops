f9087ec8216b52c7b8aff2192318806c64a33837
Zeberio <morandezeb@gmail.com>
2023-11-24 18:45:05 +0300

    fix errors on task

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # create a manifest that kills a process named 'killmenow'
 
-exec { 'kill':
-  command => 'pkill -f killmenow',
-  path    => ['/usr/bin', '/usr/sbin']
+ackage { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => gem,
 }
