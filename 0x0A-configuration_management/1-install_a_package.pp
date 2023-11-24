4d8f5ad74ec22183d55c9394a2fac9b603499557
Zeberio Morande <morandezeb@gmail.com>
2023-11-24 07:37:24 -0800

    try fix errors on installing flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-# install "flask" from "pip3" using Puppet
+# install_flask.pp
 
-package { 'flask':
+package { 'python3-pip':
+  ensure => present,
+}
+
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
 }
