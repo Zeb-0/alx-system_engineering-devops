31244bdc012cd74d1b488639ebe4c566ac7f8015
Zeberio <morandezeb@gmail.com>
2024-01-16 15:22:47 +0300

    pycodestyle fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,6 @@ def number_of_subscribers(subreddit):
     '''Return the total number of subscribers on a given subreddit.'''
     if subreddit is None or not isinstance(subreddit, str):
         return 0
-    
     user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = get(url, headers=user_agent)
@@ -19,4 +18,3 @@ def number_of_subscribers(subreddit):
         return results.get('data').get('subscribers')
     except Exception:
         return 0
-    
